fe2d813de2320a239d0123933c9b20ce19e2439f
extezyn <romafalko2006@gmail.com>
2023-09-14 16:43:58 +0300

    fsf

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("я гуль");
+            for( int i=1000; i>6; i = i - 7)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
